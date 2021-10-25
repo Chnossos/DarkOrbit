cdf52bfb24c3fd249bb9638f30a48972cd537760
Chnossos <chnossos@gmail.com>
2021-10-25 12:41:24 +0200

    add: TextureManager

---
+++ src/engine/TextureManager.cpp
@@ -0,0 +1,24 @@
+/// @file   TextureManager.cpp
+/// @author Pierre Caissial
+/// @date   Created on 22/10/2021
+
+#include "TextureManager.hpp"
+
+// Project includes
+#include "../core/Exception.hpp"
+
+using namespace Engine;
+
+auto TextureManager::load(std::string name, std::filesystem::path const & path) -> sf::Texture &
+{
+    auto & texture = _textures.try_emplace(std::move(name)).first->second;
+    Core::bAssert(texture.loadFromFile(path.string()),
+                  "Failed to load font '{}' from {}", name, path.string());
+    return texture;
+}
+
+auto TextureManager::sprite(std::string const & name) const -> sf::Sprite
+{
+    Core::bAssert(_textures.contains(name), "No texture loaded for '{}'", name);
+    return sf::Sprite(_textures.at(name));
+}
